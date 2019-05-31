f0c1b0139f45990fa2165b4f2954120e4a18352a
martin <martinmojanovski@gmail.com>
2019-05-31 10:34:49 +0200

    backend

--- WebApplication1/Controllers/FiltriranjeController.cs
+++ WebApplication1/Controllers/FiltriranjeController.cs
@@ -21,10 +21,12 @@ namespace WebApplication1.Controllers
         [HttpGet]
         [Route("")]
         public IHttpActionResult GetFiltriranje([FromUri] string id_proekt = null, string id_vraboten = null, string id_status = null, string naslov = null, string ime = null, string prezime = null)
-        {
+        {if (string.IsNullOrWhiteSpace(naslov)) return BadRequest("naslov e prazen");
+            if (string.IsNullOrWhiteSpace(ime)) return BadRequest("ime e prazen");
+            if (string.IsNullOrWhiteSpace(prezime)) return BadRequest("prezime e prazen");
             try
             {
-                var response = handlerFiltriranje.HandlerGetFiltriranje(id_proekt,id_vraboten, id_status, ime, prezime, naslov);
+                var response = handlerFiltriranje.HandlerGetFiltriranje(id_proekt,id_vraboten, id_status, ime.Trim(), prezime.Trim(), naslov.Trim());
                 return Ok(response);
             }
             catch (Exception e)
